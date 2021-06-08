30365d1823ef4dbe13600a50982db955d5d98da6
Pham Long Huan <huanvip455@gmail.com>
2021-06-08 10:03:29 +0700

    finish

--- index.js
+++ index.js
@@ -11,49 +11,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 /** Connect to database */
 const db = require('./src/core/connectDb');
 db.connect();
-/** get data from database */
+/** require model data from database */
 const temperatureModel = require('./src/models/temperatureModel');
-// ProjectModel.find({projectName: 'name'}).sort({viewCount: -1}).limit(5).exec( 
-//     function(err, projects) {
-//         ...
-//     }
-// );
 
-/** MQTT */
-const mqtt = require('mqtt');
-const { createCipher } = require('crypto');
-const client = mqtt.connect('mqtt://mqtt.flespi.io:1883', {
-    username: 'FlespiToken OsiCBKm6ehY9L9iPXHCLLqxSBxlXdJUR22htDrpFzbCa8LNViguSuSAWfLJgkrVa'
-});
 
-const TOPIC = '/topic/qos0';
+const server = require('http').Server(app);
 
-client.on('connect', function () {
-    console.log(`[MQTT] connected to MQTT broker`);
-    client.subscribe(TOPIC);
-});
-
-client.on('message', async function (topic, message) {   //khi có message đến
-    console.log(`[MQTT] message: ${message.toString()}`);
-    try {
-        const temperature = new temperatureModel({ temperature: message });
-        await temperature.save();
-        console.log(`[SERVER] data is saved`);
-        const data = await temperatureModel.find().sort({ 'createdAt': 'DESC' }).limit(5).select('temperature -_id'); //lấy 5 giá trị mới nhất từ data base
-        console.log(`[SERVER] Data get: ${data}`);
-
-    } catch (error) {
-        console.log(`[SERVER] can't save: ${error}`);
-    }
-})
 
 /** Socket IO */
-const server = require('http').Server(app);
 const io = require('socket.io')(server);
-
 io.on('connection', async socket => {
     try {
-        const data = await temperatureModel.find();
+        const data = await temperatureModel.find().sort({ 'createdAt': 'DESC' }).limit(10).select('temperature createdAt -_id'); //lấy 5 giá trị mới nhất từ data base
         console.log(`[SOCKET] co nguoi ket noi: ${socket.id}`);  //Có client kết nối thì in id của client đó ra
         socket.emit('First-data', data);
 
@@ -74,7 +43,36 @@ io.on('connection', async socket => {
     } catch (error) {
         console.log(`Internal server: ${error.message}`);
     }
+});
+
+/** MQTT */
+const mqtt = require('mqtt');
+const client = mqtt.connect('mqtt://mqtt.flespi.io:1883', {
+    username: 'FlespiToken OsiCBKm6ehY9L9iPXHCLLqxSBxlXdJUR22htDrpFzbCa8LNViguSuSAWfLJgkrVa'
+});
+
+const TOPIC = '/topic/qos0';
+
+client.on('connect', function () {
+    console.log(`[MQTT] connected to MQTT broker`);
+    client.subscribe(TOPIC);
+});
+
+client.on('message', async function (topic, message) {   //khi có message đến
+    console.log(`[MQTT] message: ${message.toString()}`);
+    try {
+        const temperature = new temperatureModel({ temperature: message });
+        await temperature.save();
+        console.log(`[SERVER] data is saved`);
+        const data = await temperatureModel.find().sort({ 'createdAt': 'DESC' }).limit(10).select('temperature createdAt -_id'); //lấy 5 giá trị mới nhất từ data base
+        io.sockets.emit('Server-send-data', data);
+        console.log(`[SERVER] Data get: ${data}`);
+
+    } catch (error) {
+        console.log(`[SERVER] can't save: ${error}`);
+    }
 })
+
 
 /** Server */
 app.get('/', (req, res) => {
--- public/js.js
+++ public/js.js
@@ -1,16 +1,90 @@
 var socket = io('http://localhost:3000');
 
-var database;
+// var database;
 
 socket.on('Server-send-data', data => {
-    console.log(data);
+    // var database = data;
+    rawChart(data.reverse());
+    // console.log(data.reverse());
 })
 socket.on('First-data', data => {
-    database = data;
-    console.log(data);
+    // database = data;
+    rawChart(data.reverse());
+    // console.log(data);
 })
 $(document).ready(() => {
     $('.btn').click(() => {
-        socket.emit('Client-send-data', "huan bo doi");
-    })
+
+
+    });
+
 });
+
+function rawChart(data) {
+
+    var tempArr = data.map(value => value.temperature);
+    var timeArr = data.map(value => {
+        var re = /(?<=T)\d+|(?<=:)\d+/g;
+        var arr = value.createdAt.match(re);
+        return `${arr[0]}:${arr[1]}:${arr[2]}`;
+    });
+    console.log(timeArr);
+    console.log(tempArr);
+
+    //Qủa biểu đồ
+    let myChart = document.getElementById('myChart').getContext('2d');
+    // Global Options
+    Chart.defaults.global.defaultFontFamily = 'Lato';
+    Chart.defaults.global.defaultFontSize = 15;
+    Chart.defaults.global.defaultFontColor = '#777';
+
+    let massPopChart = new Chart(myChart, {
+        type: 'line', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+        data: {
+            labels: timeArr,
+            datasets: [{
+                label: 'temperature',
+                data: tempArr,
+                //backgroundColor:'green',
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)',
+                    'rgba(255, 159, 64, 0.6)',
+                    'rgba(255, 99, 132, 0.6)'
+                ],
+                borderWidth: 2,
+                borderColor: '#c04000',
+                hoverBorderWidth: 3,
+                hoverBorderColor: '#000'
+            }]
+        },
+        options: {
+            title: {
+                display: true,
+                text: 'Biểu đồ nhiệt độ đo đo từ ESP',
+                fontSize: 25
+            },
+            legend: {
+                display: true,
+                position: 'right',
+                labels: {
+                    fontColor: '#000'
+                }
+            },
+            layout: {
+                padding: {
+                    left: 50,
+                    right: 0,
+                    bottom: 0,
+                    top: 40
+                },
+            },
+            tooltips: {
+                enabled: true
+            }
+        }
+    });
+}
